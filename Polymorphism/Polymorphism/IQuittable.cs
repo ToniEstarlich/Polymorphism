4afbb89b26383aa656a73a39df01d6f067ac2e45
Antonio Estarlich <110102891+ToniEstarlich@users.noreply.github.com>
2023-12-09 14:09:28 +0000

    Add files via upload

---
+++ Polymorphism/Polymorphism/IQuittable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+// Step 1: Create an interface called IQuittable
+interface IQuittable
+{
+    void Quit();
+}
+
+// Step 2: Inherit the IQuittable interface in the Employee class
+class Employee : IQuittable
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public void Quit()
+    {
+        Console.WriteLine($"{FirstName} {LastName} is quitting the job.");
+    }
+}
---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+ class Program
+    {
+        static void Main()
+        {
+            // Step 3: Use polymorphism to create an object of type IQuittable and call the Quit() method on it.
+            // In this exasmple, an Employe object is treated as an IQuitable type.
+            IQuittable quittableEmployee = new Employee
+            {
+                FirstName = "John",
+                LastName = "Doe",
+            };
+            // Call the Quit() method on the IQuittable object.
+            quittableEmployee.Quit();
+
+            // Keep the console window open to see the result.
+            Console.ReadLine();
+        }
+    }
