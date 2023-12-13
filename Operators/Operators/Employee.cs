ba767f1f6e46b4b790f5110db38f313c820f2cd3
Antonio Estarlich <110102891+ToniEstarlich@users.noreply.github.com>
2023-12-13 15:27:23 +0000

    Add files via upload

---
+++ Operators/Operators/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+    //Step 1: Create an  Employee class with Id, FirsName, and LastName properties.
+    public class Employee
+    {
+        // Properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Comstructor
+        public Employee( int id, string frstName, string lastName)
+        {
+            Id = id;
+            FirstName = FirstName;
+            LastName = LastName;
+        }
+
+        // Step: Overload the "==" operator to check if two Employe objects are equal by comparing their Id property.
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            // Check if both objects are null or if their Id properties are equal
+            return ReferenceEquals(employee1, null) && ReferenceEquals(employee2, null) ||
+                  !ReferenceEquals(employee1, null) && !ReferenceEquals(employee2, null) &&
+                   employee1.Id == employee2.Id;
+        }
+
+        // Overload the "!=" operator to complement the "==" operator.
+        public static bool operator  !=(Employee employee1, Employee employee2)
+        {
+            return !(employee1 == employee2);
+        }
+    }
---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        // Sample usage of the Employee class and overloaded operators 
+        Employee employee1 = new Employee(1, "John", "Doe");
+        Employee employee2 = new Employee(2, "Jane", "Doe");
+        Employee employee3 = new Employee(1, "John", "Doe");
+    
+
+        // Check equality using overloaded ==
+        Console.WriteLine($"employee1 == employee2: {employee1 == employee2}");
+        Console.WriteLine($"employee1 == employee3: {employee1 == employee3}");
+
+        // CHeck inequality using overloaded !=
+        Console.WriteLine($"employee1 != employee2: {employee1 != employee2}");
+        Console.WriteLine($"employee1 != employee3: {employee1 != employee3}");
+
+    }
+ }
